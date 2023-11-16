1e4d4c16a25f0f6709c34284583d0c9ef848e77b
Hugo Heynard <44136168+hugoheynard@users.noreply.github.com>
2023-11-16 18:39:24 +0100

    "test button"

---
+++ script.js
@@ -0,0 +1,15 @@
+const  displayPost  = require("./my_JS_modules/blogFunctions/displayPosts");
+const db = require("./my_JS_modules/dbFunctions/dbConnection");
+
+
+
+const getArticle = () => {
+    //db.each()  tous les posts
+}
+
+const testclick = () => {
+    displayPost(test, parent)
+}
+
+testclick()
+
