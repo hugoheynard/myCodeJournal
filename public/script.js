57edc101c049c322743c37d6a2f0305cae42c6c8
Hugo Heynard <44136168+hugoheynard@users.noreply.github.com>
2023-11-26 15:27:38 +0100

    Design base with responsive on highlight articles and articles section

--- public/script.js
+++ public/script.js
@@ -35,3 +35,21 @@ const test = async () => {
 
 test();
 
+const boxes = document.querySelectorAll(".box");
+
+window.addEventListener("scroll", DisplayContent);
+DisplayContent();
+
+function DisplayContent() {
+  const TriggerBottom = (window.innerHeight / 5) * 4;
+
+  boxes.forEach((box) => {
+    const topBox = box.getBoundingClientRect().top;
+
+    if (topBox < TriggerBottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
+}
